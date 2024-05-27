7c8d3b13140583fbfa8cf2fefcd21b571922c48a
Sirisha Kalivarapu <kalivarapusirisha62017@gmail.com>
2024-05-27 16:56:33 -0500

    build with parcel

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("div",{id:"parent",xyz:"just a attribute"},[
     React.createElement("h2",{id:"child1",xyz:"just a attribute"},[
-        React.createElement("h3",{id:"heading",xyz:"just a attribute"},"child1 kid1"),
-        React.createElement("h3",{id:"heading",xyz:"just a attribute"},"child1 kid2")
+        React.createElement("h3",{id:"heading",xyz:"just a attribute"},"learning React"),
+        React.createElement("h3",{id:"heading",xyz:"just a attribute"},"from Namaste React")
     ]),
     React.createElement("h2",{id:"child2",xyz:"just a attribute"},[
         React.createElement("h3",{id:"heading",xyz:"just a attribute"},"child2 kid1"),
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-React Application
+# React Application 💥
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR (hot module replacement)
+- uses File Matching Algorithm written in c++ 
+- it gives you faster build as it is doing Caching (parcel-cache)
+- Image Optimization (loading images to page is costly and parcel optimizes it)
+- Minification (minify our files in production)
+- Bundling (its a bundler)
+- Compressing (compresses the files and ships to production)
