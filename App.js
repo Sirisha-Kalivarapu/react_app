5d7da6a86e50f435a781b99c1044514ceca09296
Sirisha Kalivarapu <kalivarapusirisha62017@gmail.com>
2024-05-27 20:03:06 -0500

    add. jsx inplace of createElement

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("div",{id:"parent",xyz:"just a attribute"},[
-    React.createElement("h2",{id:"child1",xyz:"just a attribute"},[
-        React.createElement("h3",{id:"heading",xyz:"just a attribute"},"learning React"),
-        React.createElement("h3",{id:"heading",xyz:"just a attribute"},"from Namaste React")
-    ]),
-    React.createElement("h2",{id:"child2",xyz:"just a attribute"},[
-        React.createElement("h3",{id:"heading",xyz:"just a attribute"},"child2 kid1"),
-        React.createElement("h3",{id:"heading",xyz:"just a attribute"},"child2 kid2")
-    ])]);
-// const heading= React.createElement("h1",{id:"heading",xyz:"just a attribute"},"hello React from react");
+// create jsx which creates react element
+// Parcel uses Babel which is js compiler which converts jsx to react create element , which is converted to js object => HTML element(while rendering)
 
+const heading = <h1 id ="head" className="heading">hello react from jsx💥</h1>
+// const heading= React.createElement("h1",{id:"heading",xyz:"just a attribute"},"hello React from react");
+console.log(heading);
 const root= ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
