5e6d2c50120a5598bbad0f9103387235e230f511
Sirisha Kalivarapu <kalivarapusirisha62017@gmail.com>
2024-06-07 11:07:37 -0500

    add. shimmer component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,22 @@
 import { RestroCard } from "./RestroCard";
 import { restList } from "../utils/RestroData";
 import { useState,useEffect } from "react";
+import Shimmer from "./Shimmer";
 const Body= ()=>{
     const [listOfRestaurants,setListOfRestaurants]=useState([]);
     const fetchData= async()=>{
         const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
-        // console.log(json.data);
         console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
         setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         console.log(listOfRestaurants);
 
     }
-    useEffect(()=>{fetchData();},[])
-    // useEffect(fetchData(),[]);
+    useEffect(()=>{fetchData();},[]);
+    if(listOfRestaurants.length===0){
+        return <Shimmer />;
+    }
+        
     return(
 <div className="body-container"> 
 <div className="filter">
