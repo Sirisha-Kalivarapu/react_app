4654b633878533e123f303df278840ab8c1658c8
Sirisha Kalivarapu <kalivarapusirisha62017@gmail.com>
2024-06-07 11:48:23 -0500

    add. login button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import { LOGO_URL } from "../utils/constants";
-const Header=()=>(<div className="headContainer">
+import { useState } from "react";
+const Header=()=>{
+    const [btnName,setBtnName]=useState('Login');
+    return (<div className="headContainer">
     <div className= "logo-container">
         <img className="logo" src= {LOGO_URL}></img>
     </div>
@@ -9,7 +12,14 @@ const Header=()=>(<div className="headContainer">
             <li>AboutUs</li>
             <li>ContactUs</li>
             <li>Cart</li>
+            <button className="login-button" onClick={()=>{btnName==="Login"?setBtnName("Logout"):setBtnName("Login");
+            }}>{btnName}</button>
+
         </ul>
+        
     </div>
-</div>)
+    {/* <div className="login">
+        <button className="login-button">Login</button>
+    </div> */}
+</div>)}
 export default Header;
