ebae7a789ab3de1b0df83cb5a993a4481a72e40a
Sirisha Kalivarapu <kalivarapusirisha62017@gmail.com>
2024-06-12 09:20:53 -0500

    Add. useState in UserClass

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,11 +3,21 @@ import React from "react";
 class UserClass extends React.Component{
 constructor(props){
     super(props);
+    this.state={
+        count:1,
+    }
 }
 render(){
     const {name,location,email}=this.props;
+    const {count}=this.state;
     return (
     <div className="user-class">
+        <h4>class1: {count}</h4>
+        <button onClick={()=>{
+            this.setState({
+                count:this.state.count+1,
+            })
+        }}>Count Increment</button>
         <h2>Name: {name}</h2>
         <h2>Location: {location}</h2>
         <h2>Email: {email}</h2>
